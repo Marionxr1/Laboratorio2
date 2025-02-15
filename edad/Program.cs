bfda551ebae0aeaab4164b381df4ae2f9bc457fe
Mario Rivera <mriveras10@miumg.edu.gt>
2025-02-14 21:09:03 -0600

    Laboratorio2

---
+++ edad/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Pedir al usuario que ingrese su edad
+        Console.WriteLine("Introduce tu edad:");
+        int edad = int.Parse(Console.ReadLine());
+
+        // Determinar la categoría utilizando el switch
+        string categoria;
+
+        switch (edad)
+        {
+            case int n when (n < 12):
+                categoria = "Niño";
+                break;
+            case int n when (n >= 12 && n <= 17):
+                categoria = "Adolescente";
+                break;
+            case int n when (n >= 18 && n <= 59):
+                categoria = "Adulto";
+                break;
+            case int n when (n >= 60):
+                categoria = "Adulto mayor";
+                break;
+            default:
+                categoria = "Edad no válida";
+                break;
+        }
+
+        // Mostrar la categoría
+        Console.WriteLine($"Tu categoría es: {categoria}");
+    }
+}
+
